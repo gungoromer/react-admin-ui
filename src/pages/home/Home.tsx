f69869bb0f93a1c938b9dfeca04723b8cbb1201e
OYENAL <OYENAL@DESKTOP-9PDS8TP>
2023-12-02 03:13:00 +0300

    css grid system added to home page

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,11 +1,22 @@
-import "./Home.scss"
+import TopBox from "../../components/topbox/TopBox";
+import "./Home.scss";
 
 const Home = () => {
   return (
-    <div className='home'>
-      Home
+    <div className="home">
+      <div className="box box1">
+        <TopBox></TopBox>
+      </div>
+      <div className="box box2">Box2</div>
+      <div className="box box3">Box3</div>
+      <div className="box box4">Box4</div>
+      <div className="box box5">Box5</div>
+      <div className="box box6">Box6</div>
+      <div className="box box7">Box7</div>
+      <div className="box box8">Box8</div>
+      <div className="box box9">Box9</div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
