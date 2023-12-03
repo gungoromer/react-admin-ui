8352c9c45c59ce3e94b4c43d8c78c20fd35a95a4
Ömer GÜNGÖR <omer.gungor@wsrlife.com>
2023-12-03 15:51:34 +0300

    Add fscreen library for fullscreen functionality

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -3,7 +3,7 @@ import "./footer.scss"
 const Footer = () => {
   return (
     <div className="footer">
-      <span>lamadmin</span>
+      <span>gungoromer</span>
       <span>© Lama Dev Admin Dashboard</span>
     </div>
   )
--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,19 @@
+import fscreen from "fscreen"
 import "./navbar.scss";
 
 const Navbar = () => {
+  const handleFullscreen = () => {
+    fscreen.requestFullscreen(document.documentElement);
+  }
+
   return (
     <div className="navbar">
       <div className="logo">
         <img src="logo.svg" alt="" />
-        <span>lamadmin</span>
+        <span>gungoromer</span>
       </div>
       <div className="icons">
-        <img src="/search.svg" alt="" className="icon" />
-        <img src="/app.svg" alt="" className="icon" />
-        <img src="/expand.svg" alt="" className="icon" />
-        <div className="notification">
-          <img src="/notifications.svg" alt="" />
-          <span>1</span>
-        </div>
+        <img src="/expand.svg" alt="" className="icon" onClick={handleFullscreen} />
         <div className="user">
           <img
             src="https://images.pexels.com/photos/11038549/pexels-photo-11038549.jpeg?auto=compress&cs=tinysrgb&w=1600&lazy=load"
@@ -22,7 +21,6 @@ const Navbar = () => {
           />
           <span>Jane</span>
         </div>
-        <img src="/settings.svg" alt="" className="icon" />
       </div>
     </div>
   );
