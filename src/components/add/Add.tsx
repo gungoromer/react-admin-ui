7ce71f519d6ea625d5ca4644a982cdc2643e3565
Ömer GÜNGÖR <omer.gungor@wsrlife.com>
2023-12-25 16:02:14 +0300

    Add useMutation and useQueryClient for API integration

--- src/components/add/Add.tsx
+++ src/components/add/Add.tsx
@@ -1,6 +1,7 @@
 import { GridColDef } from "@mui/x-data-grid";
 import "./add.scss";
-// import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import ProductCategoryApi from "../../shared/Api/ProductCategory/ProductCategoryApi";
 
 type Props = {
   slug: string;
@@ -11,38 +12,26 @@ type Props = {
 const Add = (props: Props) => {
   // TEST THE API
 
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
-  // const mutation = useMutation({
-  //   mutationFn: () => {
-  //     return fetch(`http://localhost:8800/api/${props.slug}s`, {
-  //       method: "post",
-  //       headers: {
-  //         Accept: "application/json",
-  //         "Content-Type": "application/json",
-  //       },
-  //       body: JSON.stringify({
-  //         id: 111,
-  //         img: "",
-  //         lastName: "Hello",
-  //         firstName: "Test",
-  //         email: "testme@gmail.com",
-  //         phone: "123 456 789",
-  //         createdAt: "01.02.2023",
-  //         verified: true,
-  //       }),
-  //     });
-  //   },
-  //   onSuccess: () => {
-  //     queryClient.invalidateQueries([`all${props.slug}s`]);
-  //   },
-  // });
+  const mutation = useMutation({
+    mutationFn: () => {
+      var productCateogryApi = new ProductCategoryApi();
+      return productCateogryApi.create({
+        Name: "test",
+        IsActive: true,
+      });
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries([`all${props.slug}s`]);
+    },
+  });
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
     //add new item
-    // mutation.mutate();
+    mutation.mutate();
     props.setOpen(false);
   };
   return (
